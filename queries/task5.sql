b84c43ac902c01bcee9c1b6a29285279717fa4d5
divyaguddadamani <divyaguddadamani.xworkz@gmail.com>
2024-08-08 22:35:18 +0530

    sql files

---
+++ queries/task5.sql
@@ -0,0 +1,26 @@
+use divya;
+create table transaction_info (t_id int, t_type varchar(30),amount_send bigint,utr_no varchar(30),transaction_by varchar(40),bal bigint);
+
+select *from transaction_info;
+insert into transaction_info values(1,'phonepay',500,'2001BN5123','divya',1200);
+insert into transaction_info values(2,'googlepay',400,'2002BN5123','darshan',1500);
+insert into transaction_info values(3,'paytm',300,'2003BN5123','deepak',1550);
+insert into transaction_info values(4,'bank',550,'2004BN5123','kalpana',245);
+insert into transaction_info values(5,'amazonpay',250,'2005BN5123','shrikant',260);
+insert into transaction_info values(6,'googlepay',60,'2006BN5123','madhu',20);
+insert into transaction_info values(7,'phonepay',124,'2007BN5123','khushi',130);
+insert into transaction_info values(8,'paytm',25,'2008BN5123','rishi',404);
+insert into transaction_info values(9,'phonepay',220,'2009BN5123','vikas',206);
+insert into transaction_info values(10,'bank',12,'2010BN5123','kiran',666);
+insert into transaction_info values(11,'phonepay',40,'2011BN5123','arun',555);
+insert into transaction_info values(12,'phonepay',100,'2012BN5123','akash',789);
+insert into transaction_info values(13,'paytm',80,'2013BN5123','iranna',123);
+insert into transaction_info values(14,'phonepay',120,'2014BN5123','vidya',234);
+insert into transaction_info values(15,'phonepay',181,'2015BN5123','vinayak',345);
+insert into transaction_info values(16,'googlepay',190,'2151BN5123','akshay',456);
+insert into transaction_info values(17,'phonepay',520,'2016BN5123','varun',567);
+insert into transaction_info values(18,'amazonpay',500,'2017BN5123','vani',678);
+insert into transaction_info values(19,'phonepay',264,'2018BN5123','rani',789);
+insert into transaction_info values(20,'upi',212,'2019BN5123','geeta',890);
+insert into transaction_info values(20,'upi',212,'2019BN5123','geeta',890);
+
